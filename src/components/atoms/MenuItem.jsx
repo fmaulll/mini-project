45fb78e03d0a9e27b78d8e5c47a5d4e4cdb4da6b
fmaulll <maul2821@gmail.com>
2023-01-08 15:37:39 +0700

    validation for move item

--- src/components/atoms/MenuItem.jsx
+++ src/components/atoms/MenuItem.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-const MenuItem = ({ icon, children, onClick, type = "normal" }) => {
+const MenuItem = ({ disabled, icon, children, onClick, type = "normal" }) => {
   return (
     <div
-      onClick={onClick}
-      className={`w-full flex items-center px-[22px] py-2 cursor-pointer duration-200 text-sm text-dark font-semibold ${
-        type === "delete" ? "hover:text-red" : " hover:text-primary"
+      onClick={disabled ? () => {} : onClick}
+      className={`w-full flex items-center px-[22px] py-2 duration-200 text-sm ${
+        disabled ? "text-gray-300 cursor-not-allowed" : "text-dark cursor-pointer"
+      } font-semibold ${
+        type === "delete"
+          ? "hover:text-red"
+          : disabled
+          ? ""
+          : "hover:text-primary"
       }`}
     >
       {icon ? <div className="mr-[22px]">{icon}</div> : null}
--- src/components/molecules/Menu.jsx
+++ src/components/molecules/Menu.jsx
@@ -1,21 +1,35 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useOutside } from "../../hooks/useOutside";
 import MenuItem from "../atoms/MenuItem";
 import { ImArrowRight2, ImArrowLeft2 } from "react-icons/im";
 import { BiEditAlt } from "react-icons/bi";
 import { FaRegTrashAlt } from "react-icons/fa";
 
-const Menu = ({ item, onClose, handleClickMenu }) => {
+const Menu = ({ todos = [], item, onClose, handleClickMenu }) => {
   const ref = useRef(null);
+  const [position, setPosition] = useState(null);
   useOutside(ref, onClose);
+
+  const getPosition = () => {
+    const todo = todos.findIndex((todo) => todo.id === item.todo_id);
+    setPosition(todo);
+  };
+
+  useEffect(() => {
+    getPosition();
+  });
   return (
     <div
       ref={ref}
       className="absolute z-10 right-0 bg-white rounded-lg shadow-2xl py-[14px] w-[320px]"
     >
       <MenuItem
+        disabled={position === todos.length - 1}
         onClick={() => {
-          handleClickMenu("right", item);
+          handleClickMenu("right", {
+            ...item,
+            target_todo_id: todos[position + 1].id,
+          });
           onClose();
         }}
         icon={<ImArrowRight2 />}
@@ -23,8 +37,12 @@ const Menu = ({ item, onClose, handleClickMenu }) => {
         Move Right
       </MenuItem>
       <MenuItem
+        disabled={position === 0}
         onClick={() => {
-          handleClickMenu("left", item);
+          handleClickMenu("left", {
+            ...item,
+            target_todo_id: todos[position - 1].id,
+          });
           onClose();
         }}
         icon={<ImArrowLeft2 />}

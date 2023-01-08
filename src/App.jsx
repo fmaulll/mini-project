ab57ab1d365f80ff329b2793420e0fc65abe3dc7
fmaulll <maul2821@gmail.com>
2023-01-08 16:41:43 +0700

    new library for drag and drop

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout";
 import TodoPage from "./pages/TodoPage";
@@ -7,13 +9,15 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/v1/" element={<TodoPage/>} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <DndProvider backend={HTML5Backend}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/v1/" element={<TodoPage />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </DndProvider>
   );
 }
 
